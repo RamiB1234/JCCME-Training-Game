d27e6f4642792e6f13bde3a5a5303dc4fd153518
RamiB1234 <rbisthebest@hotmail.com>
2020-11-10 11:34:04 +0300

    Work on the first level design, still in progress

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -22,12 +22,29 @@ public class CameraController : MonoBehaviour
     // LateUpdate is called after Update each frame
     void FixedUpdate()
     {
-        if(target.GetComponent<Rigidbody2D>().velocity.x >0 && target.transform.position.x >= movingLimit.transform.position.x)
-        {
-            Vector3 posNoZ = transform.position;
-            posNoZ.z = target.transform.position.z;
+        Vector3 posNoZ = transform.position;
 
-            Vector3 targetDirection = (target.transform.position - posNoZ);
+        posNoZ.z = target.transform.position.z;
+
+        Vector3 targetDirection = (target.transform.position - posNoZ);
+
+        if (target.GetComponent<Rigidbody2D>().velocity.x >0 && target.transform.position.x >= movingLimit.transform.position.x)
+        {
+
+
+
+            interpVelocity = targetDirection.magnitude * 5f;
+
+            targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
+
+            transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
+        }
+        else
+        {
+            // Lerp Y
+            var lerpYPos = new Vector3(transform.position.x, target.transform.position.y, transform.position.z);
+            posNoZ.z = lerpYPos.z;
+            targetDirection = (lerpYPos - posNoZ);
 
             interpVelocity = targetDirection.magnitude * 5f;
 
